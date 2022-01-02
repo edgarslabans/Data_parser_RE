b4e4cd1d7c0a2890b89527278bfb4b81c8e1e5d5
edgarslabans <edgars.labans@gmail.com>
2022-01-03 01:08:15 +0200

    parsing of all regions

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ import os
 import lxml
 import re
 import time
+import random
 
 
 # ua = UserAgent()
-#region_of_interest = ["aizkraukle", "aluksne", "balvi", "bauska", "cesis", "daugavpils", "dobele", "gulbene", "jelgava",
- #                     "kraslava", "kuldiga", "jekabpils", "liepaja", "limbazi", "ludza", "madona", "ogre", "preili",
- #                     "rezekne", "saldus", "talsi", "tukums", "valka", "valmiera", "ventspils"]
+region_of_interest = ["aizkraukle", "aluksne", "balvi", "bauska", "cesis", "daugavpils", "dobele", "gulbene", "jelgava",
+                      "kraslava", "kuldiga", "jekabpils", "liepaja", "limbazi", "ludza", "madona", "ogre", "preili",
+                      "rezekne", "saldus", "talsi", "tukums", "valka", "valmiera", "ventspils"]
 
-region_of_interest = ["aizkraukle", "aluksne"]
+#region_of_interest = ["aluksne"]
 
 
 def parse_all():
@@ -27,7 +28,7 @@ def get_all_pages(region):
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
     }
-    r = requests.get(url="https://www.ss.lv/lv/real-estate/plots-and-lands/"+region+"rezekne""-and-reg/sell/page1.html", headers=headers)
+    r = requests.get(url="https://www.ss.lv/lv/real-estate/plots-and-lands/"+region+"-and-reg/sell/page1.html", headers=headers)
 
     if not os.path.exists("data"):
         os.mkdir("data")
@@ -35,8 +36,7 @@ def get_all_pages(region):
     with open("data/page_1.html", "w", encoding="utf-8") as file:
         file.write(r.text)
 
-    print("Processing ", region)
-    time.sleep(5)
+
 
 # extract useful data from saved html and save in json file
 def collect_data(region):
@@ -47,7 +47,9 @@ def collect_data(region):
         soup = BeautifulSoup(src, "lxml")
         table_rows = soup.find_all("tr", id=re.compile("^tr_"))  # "tr_"
 
-        print("Ņumber or rows", len((table_rows)))
+        print("Processing: ", region," Number of rows find: ", len(table_rows))
+        time.sleep(4+random.randint(0, 4))
+
 
     for item in table_rows:
         td_tags = item.find_all("td")
@@ -58,6 +60,7 @@ def collect_data(region):
             data.append(
                 {
                     "regio": region,
+                    "descr": td_tags[2].text,
                     "address": td_tags[3].text,
                     "area": td_tags[4].text,
                     "price1m2": td_tags[5].text,
@@ -65,8 +68,8 @@ def collect_data(region):
                 }
             )
 
-            with open("data.json", "a", encoding='utf8') as file:
-                json.dump(data, file, indent=4, ensure_ascii=False)
+    with open("data.json", "a", encoding='utf8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 # maximal number of pages per region - currently not in use
@@ -86,6 +89,7 @@ def main():
 
     parse_all()
 
+    #("aluksne")
 
 
 # Press the green button in the gutter to run the script.
