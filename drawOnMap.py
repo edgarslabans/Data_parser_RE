13a080b63359ab4720dbb3dafe813a266210b0ae
edgarslabans <edgars.labans@gmail.com>
2022-01-10 01:41:33 +0200

    Function of adding only new adds added

--- drawOnMap.py
+++ drawOnMap.py
@@ -1,3 +1,5 @@
+import json
+
 import pandas as pd
 
 import numpy as np
@@ -5,34 +7,40 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
 
-
 def test_draw():
     from mpl_toolkits.basemap import Basemap
     import matplotlib.pyplot as plt
 
-    #map = Basemap(projection='cyl',
-                  #lat_0=56, lon_0=0)
+    # map = Basemap(projection='cyl',
+    # lat_0=56, lon_0=0)
+    lon = []
+    lat = []
+    pricePerm2 = []
+
+    with open('data2.json', encoding='utf8') as json_file:
+        dati = json.loads(json_file.read())
+
+    for i in range(len(dati)):  #
+
+        lon.append(dati[i]['coord'][1])
+        lat.append(dati[i]['coord'][0])
+        pricePerm2.append(float(dati[i]['price1m2'][: - 2])*50)      # remove Eur symbol
+
+        print(i, float(dati[i]['price1m2'][: - 2])*100)
 
     map = Basemap(projection='lcc', resolution='i',
-                lat_0=57, lon_0=24,
-                width=0.5E6, height=0.6E6)
+                  lat_0=57, lon_0=24,
+                  width=0.5E6, height=0.6E6)
 
     map.shadedrelief()
     map.drawcoastlines(color='gray')
     map.drawcountries(color='gray')
     map.drawstates(color='gray')
 
-
-    lon = [25.03262 ,21.5635991]
-    lat = [56.362693, 57.3903918]
-
-    price = [500,600]
-    area = [300,400]
+    area = 50
 
     map.scatter(lon, lat, latlon=True,
-              c=price, s=area,
-              cmap='Reds', alpha=0.5)
-
-
+                c=pricePerm2, s=area,
+                cmap='Reds', alpha=0.5)
 
     plt.show()
--- main.py
+++ main.py
@@ -19,22 +19,20 @@ map_client = googlemaps.Client(api_key)
 geolocator = Nominatim(user_agent="sample app")
 
 # ua = UserAgent()
-region_of_interest = ["aizkraukle", "aluksne", "balvi", "bauska", "cesis", "daugavpils", "dobele", "gulbene", "jelgava",
-                      "kraslava", "kuldiga", "jekabpils", "liepaja", "limbazi", "ludza", "madona", "ogre", "preili",
-                      "rezekne", "saldus", "talsi", "tukums", "valka", "valmiera", "ventspils"]
+# region_of_interest = ["aizkraukle", "aluksne", "balvi", "bauska", "cesis", "daugavpils", "dobele", "gulbene", "jelgava",
+#     "kraslava", "kuldiga", "jekabpils", "liepaja", "limbazi", "ludza", "madona", "ogre", "preili",
+#     "rezekne", "saldus", "talsi", "tukums", "valka", "valmiera", "ventspils"]
 
-
-# region_of_interest = ["aluksne"]
+region_of_interest = "ludza"  # test range
 
 
 def parse_all():
     for i in range(len(region_of_interest)):
-        get_all_pages(region_of_interest[i])
         collect_data(region_of_interest[i])
 
 
 # save raw html data for the region
-def get_all_pages(region):
+def get_all_html(region):
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
     }
@@ -50,6 +48,8 @@ def get_all_pages(region):
 
 # extract useful data from saved html and save in json file
 def collect_data(region):
+    get_all_html(region)
+
     with open(f"data/page_1.html", encoding="utf-8") as file:
         data = []
         src = file.read()
@@ -63,22 +63,42 @@ def collect_data(region):
     for item in table_rows:
         td_tags = item.find_all("td")
 
-        # print("Item:",item)
+        n = 0
 
         if len(td_tags) > 4:
-            data.append(
-                {
-                    "regio": region,
-                    "descr": td_tags[2].text,
-                    "address": td_tags[3].text,
-                    "area": td_tags[4].text,
-                    "price1m2": td_tags[5].text,
-                    "totalPrice": td_tags[6].text
-                }
-            )
+            if not check_if_ad_exists(td_tags[2].text, td_tags[4].text, td_tags[5].text, td_tags[6].text):
+                n = +1
+                data.append(
+                    {
+                        "regio": region,
+                        "descr": td_tags[2].text,
+                        "address": td_tags[3].text,
+                        "area": td_tags[4].text,
+                        "price1m2": td_tags[5].text,
+                        "totalPrice": td_tags[6].text
+                    }
+                )
 
-    with open("data2.json", "a", encoding='utf8') as file:
+    with open("data3.json", "a", encoding='utf8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
+    print(n, "new ads found in ", region)
+
+
+# function to find new advertisements on the website
+def check_if_ad_exists(descr, area, price1m2, totalPrice):
+    dati = []
+    with open('data2.json', encoding='utf8') as json_file:
+        dati = json.loads(json_file.read())
+
+    new_data_point = [descr, area, price1m2, totalPrice]
+    outp = False
+
+    for i in range(len(dati)):
+        existing_data_point = [dati[i]["descr"], dati[i]["area"], dati[i]["price1m2"], dati[i]["totalPrice"]]
+        if new_data_point == existing_data_point:
+            outp = True
+
+    return outp
 
 
 # maximal number of pages per region - currently not in use
@@ -101,17 +121,17 @@ def process_json():
     for i in range(len(dati)):
         adrese = dati[i]["address"].replace("pag.", "pagasts, ")
         dati[i]['address'] = adrese
-        #dati[i]['coord'] = geocoder_test(adrese)
+        # dati[i]['coord'] = geocoder_test(adrese)
 
         if dati[i]['coord'][0] == 0:
             dati[i]['coord'] = geocoder_test(adrese)
 
-        print(i, adrese, dati[i]["totalPrice"],dati[i]['coord'])
+        print(i, adrese, dati[i]["totalPrice"], dati[i]['coord'])
 
     with open("data2.json", "w", encoding='utf8') as file:
         json.dump(dati, file, indent=4, ensure_ascii=False)
 
-    #print("Total processed: ", len(dati), "coordinate not found: ", n)
+    # print("Total processed: ", len(dati), "coordinate not found: ", n)
 
 
 def geocoder_test(adr):
@@ -121,7 +141,7 @@ def geocoder_test(adr):
     if location is not None:
         coord = location.latitude, location.longitude
         time.sleep(0.2)
-    else:                   #google maps api if local is not enoght
+    else:  # google maps api if local is not enoght
         try:
             geocode_rez = map_client.geocode(adr)
             coord = geocode_rez[0]["geometry"]["location"]["lat"], geocode_rez[0]["geometry"]["location"]["lng"]
@@ -131,14 +151,13 @@ def geocoder_test(adr):
     return coord
 
 
-
-
-
 def main():
     # parse_all()
-    #process_json()
-    #print (geocoder_test("Mazzalves pagasts, Lielmēmele"))
-    drawOnMap.test_draw()
+    # process_json()
+    # print (geocoder_test("Mazzalves pagasts, Lielmēmele"))
+    # drawOnMap.test_draw()
+
+    collect_data(region_of_interest)
 
 
 if __name__ == '__main__':
