03d4dc27c87d49f1e126266a6450b831127def0b
edgarslabans <edgars.labans@gmail.com>
2022-01-15 23:55:42 +0100

    Update Telegram bot

--- Bot.py
+++ Bot.py
@@ -4,10 +4,22 @@ import random
 
 bot = telebot.TeleBot(config.telegram_token)
 
+# hosting opportunity
+# selectel.ru
 
 @bot.message_handler(content_types=["text"])
 def testfunction(message):
     bot.send_message(message.chat.id, message.text)
+
+
+#test buttons
+markup = types.InlineKeyboardMarkup()
+
+for key, value in stringList.items():
+    markup.add(types.InlineKeyboardButton(text=value,
+                                          callback_data="['value', '" + value + "', '" + key + "']"),
+               types.InlineKeyboardButton(text=crossIcon,
+                                          callback_data="['key', '" + key + "']"))
 
 
 bot.polling(none_stop=True)
